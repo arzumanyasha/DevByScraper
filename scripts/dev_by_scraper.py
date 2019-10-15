05c1c3ccadf5df02b56e8d84674d6cea69fb875f
Arzumanyasha <arzumanianartur0@gmail.com>
2019-10-15 15:30:58 +0300

    Save data to csv file

--- scripts/dev_by_scraper.py
+++ scripts/dev_by_scraper.py
@@ -1,4 +1,6 @@
 import requests
+import collections
+import csv
 from bs4 import BeautifulSoup
 
 
@@ -21,11 +23,48 @@ def get_total_pages(html):
     return int(total_pages)
 
 
+def write_csv(data):
+    with open('devby.csv', 'a') as f:
+        writer = csv.writer(f)
+
+        try:
+            writer.writerow((data['url'], data['title'], data['description']))
+        except:
+            writer.writerow(('', '', ''))
+
+
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
 
     events = soup.find('div', class_='list-item-events').find_all('div', class_='item-body')
-    print(events)
+    # print(events)
+    for event in events:
+        # url, title, description, date
+        try:
+            event_body = event.find('a', class_='title')
+            url = "https://events.dev.by/" + event_body.get('href')
+            title = event_body.get('title').split("|")[1]
+
+            description_html = get_html(url)
+            description_soup = BeautifulSoup(description_html, 'lxml')
+            descriptions = description_soup.find('div', class_='text').find_all('p')
+
+            # date = event.find('p').find('time')
+
+            # description = event_body.get('')
+        except:
+            url = ''
+            title = ''
+            descriptions = ''
+
+        # Event = collections.namedtuple('Event', ['url', 'title', 'description', 'date'])
+        # new_event = Event(url, title, descriptions, None)
+        new_event = {'url': url,
+                     'title': title,
+                     'description': descriptions,
+                     'date': None}
+        # print(new_event)
+        write_csv(new_event)
 
 
 def main():
